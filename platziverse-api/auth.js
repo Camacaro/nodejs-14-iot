8c31838cf3b7ea612541f420679f290b395f9cff
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-06-03 11:24:42 -0400

    file auth

---
+++ platziverse-api/auth.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const jwt = require('jsonwebtoken')
+
+function sign (payload, secret, callback) {
+    jwt.sign(payload, secret, callback)
+}
+
+function verify (token, secret, callback) {  
+    jwt.verify(token, secret. callback)
+}
